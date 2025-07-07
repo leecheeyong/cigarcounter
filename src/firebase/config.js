b4c3a50033a389db0f165c0ecd13f0e488b24f93
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2025-07-08 01:48:54 +0800

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQNuQrEYVE0prSkCZZKYHK-Wf6ACvsQf0",
+  apiKey: process.env.API_KEY,
   authDomain: "cigarcounter-f927f.firebaseapp.com",
   projectId: "cigarcounter-f927f",
   storageBucket: "cigarcounter-f927f.firebasestorage.app",
   messagingSenderId: "887400669927",
-  appId: "1:887400669927:web:f9da33d0dbdabfea5e3842",
+  appId: "1:887400669927:web:dfb9d3a7fc80ccb05e3842"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
