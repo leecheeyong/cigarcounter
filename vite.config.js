3ba8b79d33f388d2786e68b048bec8b3f0139c81
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2025-07-08 02:11:44 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+
+export default defineConfig(({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return {
+        plugins: [vue(), tailwindcss()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+        define: {
+            "__API_KEY__": process.env.API_KEY
+        }
+    };
+});
